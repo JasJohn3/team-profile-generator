da9487994744f075766079522a9dbc89dc1ca7d2
Your Name <jasjohn3@uat.edu>
2021-08-21 01:40:11 -0700

    Engineer and Intern Red Test Fails registered

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee');
 
 test(`creates a Engineer Object`, () => {
-  const employee = new Employee('John Doe',1,'johndoe@email.com','Engineer');
+  const employee = new Employee('John Doe',1,'johndoe@email.com');
   expect(employee.name).toBe('John Doe');
   expect(employee.id).toBe(1);
   expect(employee.role).toBe('Engineer');
