6c821cfb8438a3190d6842db91ec0cbb99fd7420
Your Name <jasjohn3@uat.edu>
2021-08-21 01:23:09 -0700

    Employee Object tests Success

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
-import Employee from '../src/Employee';
+const Employee = require('../lib/Employee');
 
 test('creates a Employee Object', () => {
-  const employee = new Employee('John Doe');
-  expect(Employee.name).toBe('John Doe');
-  expect(Employee.id).toEqual(expect.any(Number));
-  expect(Employee.email).toEqual(expect.any(String));
+  const employee = new Employee('John',1,'johndoe@email.com','Employee');
+  expect(employee.name).toBe('John');
+  expect(employee.id).toBe(1);
+  expect(employee.email).toBe('johndoe@email.com');
   expect(employee.role).toBe('Employee');
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,8 @@
-import Employee from '../src/Employee';
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-test(`Employee`, () => {
-  const employee = new Employee('John Doe',1,'johndoe@email.com');
-  expect(Employee.name).toBe('John Doe');
-  expect(Employee.id).toBe(1);
-  expect(Employee.email).toBe('johndoe@email.com');
+const Employee = require('../lib/Employee');
+test(`creates a Manager Object`, () => {
+  const employee = new Employee('John Doe',1,'johndoe@email.com', 'Manager');
+  expect(employee.name).toBe('John Doe');
+  expect(employee.id).toBe(1);
+  expect(employee.email).toBe('johndoe@email.com');
   expect(employee.role).toBe('Manager');
 });
