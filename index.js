fac1a8e0f51f6250b5da5f3ca9b7bc46fb18100d
Your Name <jasjohn3@uat.edu>
2021-08-22 00:21:49 -0700

    Creating the Inquirer prompt questions

--- index.js
+++ index.js
@@ -5,26 +5,141 @@ const path = require('path');
 const questions = [
   {
     type: 'input',
-    name: 'name',
-    message: 'What is the Employee name?',
+    name: 'manager',
+    message: 'What is the Team Manager name?',
   },
   {
     //Ask for the employee email
     type: 'input',
-    name: 'email',
-    message: 'What is the Employee email?',
+    name: 'managerEID',
+    message: 'What is the Manager Employee ID?',
+  },
+  {
+    //Ask for the employee email
+    type: 'input',
+    name: 'managerEmail',
+    message: 'What is the Manager email?',
+  },
+  {
+    //Ask for the employee email
+    type: 'input',
+    name: 'managerOID',
+    message: 'What is the Manager Office Number?',
+  },
+  {
+    type: 'confirm',
+    name: 'addEmployee',
+    message: 'Would you like to add a new team Member?',
+    default: true,
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What is the Employee role?',
+    choices: ['Engineer','Intern'],
+    when:({addEmployee}) => {
+      if (addEmployee) {
+        return true;
+      }else{
+        return false;
+      }
+    }
   },
   {
     type: 'input',
-    name: 'color',
-    message: 'What is your favorite color?',
-    default: 'World',
+    name: 'engineerName',
+    message: 'What is the Engineers Name?',
+    when: ({role}) => {
+      if (role === 'Engineer') {
+        return true;
+      }else{
+        return false;
+      }
+    }
   },
   {
     type: 'input',
-    name: 'food',
-    message: 'What is your favorite food?',
-    default: 'World',
+    name: 'engineerEID',
+    message: 'What is the Engineers Employee ID?',
+    when: ({role}) => {
+      if (role === 'Engineer') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'engineerEmail',
+    message: 'What is the Engineers Email?',
+    when: ({role}) => {
+      if (role === 'Engineer') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'engineerGithub',
+    message: 'What is the Engineers Github?',
+    when: ({role}) => {
+      if (role === 'Engineer') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'internName',
+    message: 'What is the intern Name?',
+    when: ({role}) => {
+      if (role === 'Intern') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'internEID',
+    message: 'What is the intern Employee ID?',
+    when: ({role}) => {
+      if (role === 'Intern') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'internEmail',
+    message: 'What is the intern Email?',
+    when: ({role}) => {
+      if (role === 'Intern') {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'internSchool',
+    message: 'What is the Interns school?',
+    when: ({role}) => {
+      if (role === 'Intern') {
+        return true;
+      }else{
+        return false;
+      }
+    }
   },
 ];
 
@@ -32,7 +147,7 @@ inquirer
   .prompt(questions)
   .then((answers) => {
     fs.writeFileSync(
-      path.resolve(__dirname, '../answers.json'),
+      path.resolve(__dirname, './answers.json'),
       JSON.stringify(answers)
     );
     console.log('Answers saved to answers.json');
